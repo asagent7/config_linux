470aa1b4b20fdcf0dd91c25f631db706698c938c
Akshit Sharma <asagent7@gmail.com>
2016-05-12 10:42:25 +0530

    Copying ycm config file as a hidden file

--- update_pc_files.sh
+++ update_pc_files.sh
@@ -6,5 +6,5 @@ sudo cp vimrc /etc/vim/vimrc
 cp bashrc ~/.bashrc
 cp gitconfig ~/.gitconfig
 cp git-prompt.sh ~/git-prompt.sh
-cp ycm_extra_conf.py ~/ycm_extra_conf.py
+cp ycm_extra_conf.py ~/.ycm_extra_conf.py
 cp -r .vim ~/.vim
